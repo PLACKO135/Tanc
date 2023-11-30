77cf1af06b1f2bd33e1e613b9a2092cd8e7bc85b
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2023-11-30 11:09:22 +0100

    Add files via upload

--- GyászTánc/MainWindow.xaml.cs
+++ GyászTánc/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace GyászTánc
         {
             InitializeComponent();
         }
+
+        private void txtferfi_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Enter)
+            {
+                lbferfi.Items.Add(txtferfi.Text);
+                txtferfi.Text = "";
+            }
+        }
+
+        private void txtno_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                lbno.Items.Add(txtno.Text);
+                txtno.Text = "";
+            }
+           
+        }
+
+        private void btnmix_Click(object sender, RoutedEventArgs e)
+        {
+            int tancparok=0;
+            if (lbferfi.Items.Count > lbno.Items.Count)
+            {
+                 tancparok = lbno.Items.Count;
+            }
+            else
+            {
+                tancparok = lbferfi.Items.Count;
+            }
+
+            Random r = new Random();
+
+            for (int i = 0; i < tancparok; i++)
+            {
+                int ferfi=r.Next(0,tancparok-1);
+                int no =r.Next(0,tancparok-1);
+
+                lberedmeny.Items.Add($"{lbferfi.Items[ferfi]} + {lbno.Items[no]}");
+                lbferfi.Items.RemoveAt(ferfi);
+                lbno.Items.RemoveAt(no);
+            }
+           
+        }
     }
 }
